8ea75f70a28d96ab1f8b9f222434cd77f347e866
User <User@DESKTOP-EC4BH9C>
2021-11-03 14:45:54 +0900

    걸밋

--- FinalProject/src/main/java/com/spring/univ/controller/HyunController.java
+++ FinalProject/src/main/java/com/spring/univ/controller/HyunController.java
@@ -47,7 +47,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HyunController {
 	
-	@RequestMapping(value="/test1.univ") // /test1.action의 url은 아래의 메소드가 응답함!
+	@RequestMapping(value="/test2.univ") // /test1.action의 url은 아래의 메소드가 응답함!
 	public String test1(HttpServletRequest request) {
 		
 		String name = "금길영내꺼해♥";
